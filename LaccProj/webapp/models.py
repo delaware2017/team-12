83fcafa49d480026f2f212a559d9742a1bcef894
Drive100 <Drive100@syr.edu>
2017-11-04 08:16:33 -0400

    cleaned up

--- LaccProj/webapp/models.py
+++ LaccProj/webapp/models.py
@@ -5,54 +5,36 @@ from django.db import models
 
 # Create your models here.
 class Nominee(models.Model):
-    first_name = models.CharField(max_length=100, null=False, blank=False)
-    last_name  = models.CharField(max_length=100, null=False, blank=False)
-    email = models.CharField(max_length=100, null=False, blank=False)
-    phone = models.CharField(max_length=12, null=False, blank=False)
-    gpa = models.IntegerField(default= 0, null=False ,blank=False)
-    number_of_AP_courses = models.IntegerField(default= 0,null=False, blank=False)
-    Do_you_consider_yourself_hispanic = models.NullBooleanField(null=True)
+    first_name = models.CharField(max_length=100, null=False, blank=False) #generates text box input for firstname
+    last_name  = models.CharField(max_length=100, null=False, blank=False) #generates text box input for lastname
+    email = models.CharField(max_length=100, null=False, blank=False) #generates text box for email
+    phone = models.CharField(max_length=12, null=False, blank=False) #generates text box for phone number, limited to 12 characters
+    gpa = models.IntegerField(default= 0, null=False ,blank=False) #generates text box to take an integer for GPA
+    number_of_AP_courses = models.IntegerField(default= 0,null=False, blank=False) #generates text box to take an integer for # of AP courses
+    Do_you_consider_yourself_hispanic = models.NullBooleanField(null=True) #generates drop down menu to select yes/no/unknown 
 
-    athletics = models.BooleanField()
-    #gpa_int = float(gpa)
-    #if gpa_int >= 3.91:
-    #    sum = 3
-    #elif gpa_int >= 3.75:
-     #   sum = 2
-    #elif gpa_int >= 3.5:
-    #    sum = 1
-    #else:
-     #   sum = 0
+    athletics = models.BooleanField() #generates checkbox 
 
-   # score = models.IntegerField(default= sum,null=False, blank=False)
 
-    
-    #ath = athletics.
     if (athletics == True):
-     certificates = models.CharField(max_length=100, null=False, blank=False)
+     certificates = models.CharField(max_length=100, null=False, blank=False) 
 
 
-    STEM = models.BooleanField()
+    STEM = models.BooleanField()#generates checkbox
 
     if (STEM == True):
         Any_Science_Projects_or_Awards_in_these_fields = models.CharField(max_length=200, null=False, blank=False)
 
-    arts = models.BooleanField()
-
-    #if(arts == True):
-       # Visual_art = models.FileField.upload_to()
-        #Performing_art = models.FileField.upload_to() #can be video or video link
-
-    community_Service = models.BooleanField()
-
-    if(community_Service == True):
-        community_statement = models.CharField(max_length=200, null=False, blank=False)
+    arts = models.BooleanField()#generates checkbox
 
 
-    personal_Statement = models.CharField(max_length=7500, null=False, blank=False)
-    #student_Accept = models.BooleanField()
-    Parent_or_Guardian_Name = models.CharField(max_length=50, null=False, blank=False)
-    Parent_or_guardian_accept = models.BooleanField()
+    community_Service = models.BooleanField()#generates checkbox
+
+
+    personal_Statement = models.CharField(max_length=7500, null=False, blank=False) #generates text box for personal statement, limited to 7500 characters
+    student_Accept = models.BooleanField() #generates checkbox
+    Parent_or_Guardian_Name = models.CharField(max_length=50, null=False, blank=False) #generates text box for Parents/Guardians name
+    Parent_or_guardian_accept = models.BooleanField() #generates checkbox
 
     def __str__(self):
         """this sets the default return for this object"""
