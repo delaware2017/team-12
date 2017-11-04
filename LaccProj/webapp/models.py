c67545474eb1380353a2e7a990baea84ff088eca
zachandbro <zachandbro@gmail.com>
2017-11-04 01:03:22 -0400

    Commit #3 -Kevin

--- LaccProj/webapp/models.py
+++ LaccProj/webapp/models.py
@@ -8,12 +8,20 @@ class Nominee(models.Model):
     first_name = models.CharField(max_length=100, null=False, blank=False)
     last_name  = models.CharField(max_length=100, null=False, blank=False)
     email = models.CharField(max_length=100, null=False, blank=False)
-    phone = models.IntegerField(null=False, blank=False)
-    hispanic = models.BooleanField()
+    phone = models.CharField(max_length=12, null=False, blank=False)
+    Do_you_consider_yourself_hispanic = models.NullBooleanField(null=True)
     athletics = models.BooleanField()
-    stem = models.BooleanField()
+    if (athletics):
+     certificates = models.CharField(max_length=100, null=False, blank=False)
+
+
+    STEM = models.BooleanField()
     arts = models.BooleanField()
-    community = models.BooleanField()
+    community_service = models.BooleanField()
+    personal_statement = models.CharField(max_length=7500, null=False, blank=False)
+    student_accept = models.BooleanField()
+    Parent_or_guardian_name = models.CharField(max_length=50, null=False, blank=False)
+    Parent_or_guardian_accept = models.BooleanField()
 
     def __str__(self):
         """this sets the default return for this object"""
