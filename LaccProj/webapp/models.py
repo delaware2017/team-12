3b54ee4c85fb3a51ef121684eb7bd29ec1f815df
Tommy Ma <tpm5337@psu.edu>
2017-11-04 01:45:05 -0400

    Tommy's 3commit

--- LaccProj/webapp/models.py
+++ LaccProj/webapp/models.py
@@ -9,19 +9,37 @@ class Nominee(models.Model):
     last_name  = models.CharField(max_length=100, null=False, blank=False)
     email = models.CharField(max_length=100, null=False, blank=False)
     phone = models.CharField(max_length=12, null=False, blank=False)
+    gpa = models.CharField(max_length=4, null=False, default = " ",blank=False)
+    number_of_AP_courses = models.IntegerField(default= 0,null=False, blank=False)
     Do_you_consider_yourself_hispanic = models.NullBooleanField(null=True)
     athletics = models.BooleanField()
-    if (athletics):
+    
+    #ath = athletics.
+    if (athletics == True):
      certificates = models.CharField(max_length=100, null=False, blank=False)
-        
+
 
     STEM = models.BooleanField()
+
+    if (STEM == True):
+        Any_Science_Projects_or_Awards_in_these_fields = models.CharField(max_length=200, null=False, blank=False)
+    
     arts = models.BooleanField()
-    community_service = models.BooleanField()
-    personal_statement = models.CharField(max_length=7500, null=False, blank=False)
-    student_accept = models.BooleanField()
-    Parent_or_guardian_name = models.CharField(max_length=50, null=False, blank=False)
-    Parent_or_guardian_accept = models.BooleanField()
+
+    #if(arts == True):
+       # Visual_art = models.FileField.upload_to()
+        #Performing_art = models.FileField.upload_to() #can be video or video link
+
+    community_Service = models.BooleanField()
+
+    if(community_Service == True):
+        community_statement = models.CharField(max_length=200, null=False, blank=False)
+
+
+    personal_Statement = models.CharField(max_length=7500, null=False, blank=False)
+    #student_Accept = models.BooleanField()
+    Parent_or_Guardian_Name = models.CharField(max_length=50, null=False, blank=False)
+    Parent_or_Guardian_Accept = models.BooleanField()
 
     def __str__(self):
         """this sets the default return for this object"""
